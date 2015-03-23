7152f3a1637f56af3168c11b95db34c37861ce11
pig <ckjoker78@gmail.com>
2015-03-23 22:23:29 +0800

    submit

--- Chapter2-Linked Lists/2-1/Solution.java
+++ Chapter2-Linked Lists/2-1/Solution.java
@@ -3,4 +3,14 @@ Write code to remove duplicates from an unsorted linked list.
 FOLLOW UP
 How would you solve this problem if a temporary buffer is not allowed?
 */
+import CtCILibrary.*;
+public class Solution{
 
+
+	
+public static void main(String[] args) {
+	int []a={1,2,3,4,5};
+	ListNode head=ListNode.generateList(a);
+	ListNode.printList(head);
+}
+}
--- CtCILibrary/ListNode.java
+++ CtCILibrary/ListNode.java
@@ -1,3 +1,4 @@
+package CtCILibrary;
 public class ListNode{
 	int val;
 	ListNode next;
@@ -5,8 +6,17 @@ public class ListNode{
 		val=x;
 		next=null;
 	}
-	public void addBegining(int val){
-		
+	ListNode(){
+
+	}
+	public static ListNode generateList(int []list){
+		ListNode head=new ListNode();
+		ListNode cur=head;
+        for(int elem:list){
+        	cur.next=new ListNode(elem);
+        	cur=cur.next;
+        }
+        return head.next;
 	}
 	public static void printList(ListNode head){
 		while(head!=null){
@@ -16,7 +26,7 @@ public class ListNode{
 		System.out.println();
 	}
 	public static void main(String[] args) {
-		ListNode n1=new ListNode(1);
+/*		ListNode n1=new ListNode(1);
 		ListNode n2=new ListNode(3);
 		ListNode n3=new ListNode(5);
 		ListNode n4=new ListNode(7);
@@ -28,9 +38,9 @@ public class ListNode{
 		n3.next=n4;
 		n4.next=n5;
 		n5.next=n6;
-		n6.next=n7;
-		printList(n1);    
+		n6.next=n7;*/
+		int []arr={1,3,5,7,9,2,4,6};
+		ListNode list=generateList(arr);
+		printList(list);    
 	}
-
-
 }
