6efd26457840ab1c08b18ade10c2fad608e16798
pig <ckjoker78@gmail.com>
2015-03-24 16:50:31 +0800

    remove dup 1

--- Chapter2-Linked Lists/2-1/Solution.java
+++ Chapter2-Linked Lists/2-1/Solution.java
@@ -4,13 +4,33 @@ FOLLOW UP
 How would you solve this problem if a temporary buffer is not allowed?
 */
 import CtCILibrary.*;
+import java.util.HashMap;
 public class Solution{
-
-
+/*
+Method 1:用HashMap存储节点，遇到重复就删除
+Time:O(n) Space:O(n)
+*/
+public static ListNode removeDuplicates(ListNode head){
+	HashMap<Integer,Boolean> map=new HashMap<Integer,Boolean>();
+	ListNode previous=null;
+	ListNode curr=head;
+	while(curr!=null){
+		if(map.containsKey(curr.val)){
+            previous.next=curr.next;
+		}else{
+			map.put(curr.val,true);
+			previous=curr;
+		}
+		curr=curr.next;
+	}
+	return curr;
+}
 	
 public static void main(String[] args) {
-	int []a={1,2,3,4,5};
+	int []a={1,2,3,2,4,5,1,6};
 	ListNode head=ListNode.generateList(a);
+	ListNode.printList(head);
+	removeDuplicates(head);
 	ListNode.printList(head);
 }
 }
--- CtCILibrary/ListNode.java
+++ CtCILibrary/ListNode.java
@@ -1,31 +1,47 @@
-package CtCILibrary;
-public class ListNode{
-	int val;
-	ListNode next;
-	ListNode(int x){
-		val=x;
-		next=null;
-	}
-	ListNode(){
+ package CtCILibrary;
+ public class ListNode{
+ 	public  int val;
+ 	public ListNode next;
+ 	ListNode(int x){
+ 		val=x;
+ 		next=null;
+ 	}
+ 	ListNode(){
 
-	}
-	public static ListNode generateList(int []list){
-		ListNode head=new ListNode();
-		ListNode cur=head;
-        for(int elem:list){
-        	cur.next=new ListNode(elem);
-        	cur=cur.next;
-        }
-        return head.next;
-	}
-	public static void printList(ListNode head){
-		while(head!=null){
-			System.out.print(head.val+" ");
-			head=head.next;
-		}
-		System.out.println();
-	}
-	public static void main(String[] args) {
+ 	}
+ 	public static ListNode generateList(int []list){
+ 		ListNode head=new ListNode();
+ 		ListNode cur=head;
+ 		for(int elem:list){
+ 			cur.next=new ListNode(elem);
+ 			cur=cur.next;
+ 		}
+ 		return head.next;
+ 	}
+ 	public static void printList(ListNode head){
+ 		while(head!=null){
+ 			System.out.print(head.val+" ");
+ 			head=head.next;
+ 		}
+ 		System.out.println();
+ 	}
+ 	public static ListNode deleteNode(ListNode head,int x){
+ 		if(head==null)
+ 			return head;
+ 		ListNode curr=head;
+ 		if(curr.val==x){
+ 			return head.next;
+ 		}
+ 		while(curr.next!=null){
+ 			if(curr.next.val==x){
+ 				curr.next=curr.next.next;
+ 				return head;
+ 			}
+ 			curr=curr.next;
+ 		}
+ 		return head;
+ 	}
+ 	public static void main(String[] args) {
 /*		ListNode n1=new ListNode(1);
 		ListNode n2=new ListNode(3);
 		ListNode n3=new ListNode(5);
@@ -39,8 +55,17 @@ public class ListNode{
 		n4.next=n5;
 		n5.next=n6;
 		n6.next=n7;*/
-		int []arr={1,3,5,7,9,2,4,6};
+		int []arr={5,6};
 		ListNode list=generateList(arr);
-		printList(list);    
+		printList(list); 
+		list=deleteNode(list,7);
+		printList(list);
+		list=deleteNode(list,5);
+		printList(list);
+		list=deleteNode(list,6);
+		printList(list);
+		list=deleteNode(list,6);
+		printList(list);
+
 	}
 }
