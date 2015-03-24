2a2ac6d54f5a432c91561550ea7774e021534767
pig <ckjoker78@gmail.com>
2015-03-24 18:58:19 +0800

    remove dup 2

--- Chapter2-Linked Lists/2-1/Solution.java
+++ Chapter2-Linked Lists/2-1/Solution.java
@@ -25,12 +25,42 @@ public static ListNode removeDuplicates(ListNode head){
 	}
 	return curr;
 }
-	
+/*
+Method 2:类似于Brute Force从头节点开始，每处理一个节点就遍历剩下所有的节点，并把重复的删除，然后处理下一个节点
+Time:O(n^2)  Space:O(1)
+*/
+public static ListNode removeDuplicates2(ListNode head){
+    ListNode curr=head;
+    while(curr!=null){
+       ListNode runner=curr;
+       while(runner.next!=null){
+          if(curr.val==runner.next.val){
+          	runner.next=runner.next.next;
+          }else{
+          	runner=runner.next;
+          }
+       }
+       curr=curr.next;
+    }
+    return head;  
+}	
 public static void main(String[] args) {
-	int []a={1,2,3,2,4,5,1,6};
-	ListNode head=ListNode.generateList(a);
-	ListNode.printList(head);
-	removeDuplicates(head);
-	ListNode.printList(head);
+	int []a={1,2,3,3,3,2,4,5,1,6};
+	int []b={1,2,3,3,3,2,4,5,1,6};
+	int []c={};
+	ListNode l1=ListNode.generateList(a);
+	ListNode l2=ListNode.generateList(b);
+	ListNode l3=ListNode.generateList(c);
+	ListNode.printList(l1);
+	removeDuplicates(l1);
+	removeDuplicates(l3);
+	ListNode.printList(l1);
+	ListNode.printList(l3);
+
+	ListNode.printList(l2);
+	removeDuplicates2(l2);
+	removeDuplicates2(l3);
+	ListNode.printList(l2);
+	ListNode.printList(l3);
 }
 }
