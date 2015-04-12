37f86452f291a386097df2547bf747b1ab9d095d
pig <ckjoker78@gmail.com>
2015-04-12 22:45:35 +0800

    tm

--- Chapter2-Linked Lists/2-3/Solution.java
+++ Chapter2-Linked Lists/2-3/Solution.java
@@ -1,6 +1,7 @@
 /*删除单链表的一个中间节点，只能访问那个节点（不能访问头节点）。*/
 /* 把要删除节点的下一个节点的数据赋值给要删除的节点，然后把要删除节点的下一个节点删掉
   此方法无法删除尾节点,这点需要跟面试官指出来，为了处理尾节点，可以把尾节点赋值为特殊值，本题不做处理 */
+  
 import CtCILibrary.*;
 public class Solution{
 	public static boolean deleteNode(ListNode n){
